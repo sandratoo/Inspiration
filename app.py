1a958a06353a5d08d2465e53e63efe4f99eeb176
sandratoo <toosandraa@gmail.com>
2022-05-30 19:00:21 +0300

    first commit

--- app.py
+++ app.py
@@ -1,19 +1,42 @@
-from re import T
-from flask import Flask,render_template,request,flash 
+from flask import Flask, redirect,render_template,request,flash, url_for
+from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///quote.db"
 app.secret_key = "hello"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-@app.route("/hello")
+db = SQLAlchemy(app)
+
+class Inspiration(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    inspiration = db.Column(db.String(150))
+
+    def __repr__(self):
+        return f"{self.id} - {self.inspiration}"
+
+# Lnding page
+@app.route("/")
 def index():
-    flash("What's your name?")
+    flash("Welcome! Are you here to inspire or get inspired?")
     return render_template("index.html")
 
-@app.route("/greet",methods=["POST","GET"])
-def greet():
-    name = request.form["name"]
-    flash(f"Hello {name} nice to see you!")
-    return render_template("index.html")
+# Gets an inspiration from a form and stores it in the database
+@app.route("/inspire",methods=["POST","GET"])
+def inspire():
+    inspiration = request.form["inspiration"]
+    if inspiration:
+        db.session.add(inspiration)
+        db.session.commit()
+        return redirect(url_for("/"))
+    return render_template("inspire.html")
+
+# Gets a random quote from the database
+@app.route("/getinspired",methods=["GET","POST"])
+def getinspired():
+    flash("Hope you feel inspired!")
+    return render_template("getinspired.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
